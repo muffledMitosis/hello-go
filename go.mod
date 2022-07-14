876dca1df9512a2d5d4baed606a823f4ae1c3baa
Meth Munindradasa <methsndr@gmail.com>
2022-07-14 06:33:04 +0530

    Initial Commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module muffledMitosis/hello
+
+go 1.18
+
+require muffledMitosis/greetings v0.0.0-00010101000000-000000000000
---
+++ hello.go
@@ -0,0 +1,26 @@
+// Package main provides ...
+package main
+
+import (
+  "fmt"
+  "muffledMitosis/greetings"
+  "log"
+)
+
+func main() {
+  log.SetPrefix("greetings: ")
+  // log.SetFlags(0)
+
+  message, err := greetings.Hello("Meth")
+  if err != nil {
+    log.Fatal(err)
+  }
+
+  names := []string{"Meth", "Bob", "Alice"}
+  messages, err := greetings.Hellos(names)
+  if err != nil {
+    log.Fatal(err)
+  }
+  fmt.Println(messages)
+  fmt.Println(message)
+}
